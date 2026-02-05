2ced3f42f9badf4aef6bef4c2525a2795e161a68
Qu Xin <xy3082043438@gmail.com>
2026-02-05 19:05:02 +0800

    初始化

---
+++ src/main/java/com/lamb/springaiknowledgeserver/SpringaiKnowledgeServerApplication.java
@@ -0,0 +1,13 @@
+package com.lamb.springaiknowledgeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringaiKnowledgeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringaiKnowledgeServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lamb/springaiknowledgeserver/SpringaiKnowledgeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lamb.springaiknowledgeserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringaiKnowledgeServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
